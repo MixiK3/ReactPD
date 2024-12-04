d9c8876913892aca5d47bb9fc2ae26a981333a78
Mixikk <mixik2005@mail.ru>
2024-12-05 01:37:25 +0300

    Fixing sounds

--- src/pages/Rating/RatingForClass/ClassSelection.jsx
+++ src/pages/Rating/RatingForClass/ClassSelection.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './ClassSelection.module.css'
-
+import useSound from 'use-sound'
+import soundClickForClasses from '../../../assets/mp3/soundClickForClasses.mp3'
 const ClassSelection = () => {
+  const [click] = useSound(soundClickForClasses)
   const classes = Array.from({ length: 11 }, (v, i) => i + 1)
 
   return (
@@ -13,7 +15,11 @@ const ClassSelection = () => {
       <ul className={styles.list}>
         {classes.map((classId) => (
           <li key={classId}>
-            <Link to={`/rating/classes/${classId}`} className={styles.link}>
+            <Link
+              to={`/rating/classes/${classId}`}
+              className={styles.link}
+              onClick={click}
+            >
               Класс {classId}
             </Link>
           </li>
